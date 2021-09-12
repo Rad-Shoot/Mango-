f1a11489fd9b2d60934796e8837e10cb8b00ee9c
Rad Shoot <radshoot101@gmail.com>
2021-09-12 13:36:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -96,55 +96,6 @@ setInterval(()=>{
     }
 },10000)
 
-/*Typing Animation
-function typingAnimation(){
-const textDisplay = document.querySelector('.carousel-text');
-const phrases = ['We Build Phones that Last Lifetimes', 'We Accomplish the Impossible', 'Innovate, Invent, Inspire'];
-let i = 0;
-let j = 0;
-let currentPhrase = [];
-let isDeleting = false;
-let isEnd = false;
-
-function loop(){
-  isEnd=false;
-  textDisplay.innerHTML=currentPhrase.join('');
-
-  if(i<phrases.length){
-    if(!isDeleting && j<=phrases[i].length){
-      currentPhrase.push(phrases[i][j]);
-      j++;
-      textDisplay.innerHTML=currentPhrase.join('');
-    }
-    if(isDeleting && j<=phrases[i].length){
-      currentPhrase.pop(phrases[i][j]);
-      j--;
-      textDisplay.innerHTML=currentPhrase.join('');
-    }
-
-    if(j==phrases[i].length){
-      isEnd=true;
-      isDeleting=true;
-    }
-
-    if(isDeleting && j === 0) {
-      currentPhrase=[];
-      isDeleting=false;
-      i++;
-      if(i===phrases.length) {
-        i=0;
-      }
-    }
-  }
-    const speedUp=Math.random() * (80 -50) + 50;
-    const normalSpeed=Math.random() * (300 -200) + 200;
-    const timer= isEnd ? 2000 : isDeleting ? speedUp : normalSpeed;
-    setTimeout(loop, timer)
-}
-    loop()
-}
-typingAnimation();*/
-
 //Adding card functionality for mobile
 let cards=document.querySelectorAll('.card');
 let cardContents=document.querySelectorAll('.card-content');
